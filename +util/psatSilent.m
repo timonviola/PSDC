77b7b7799530d450f88bf7e019b3646cc3fcf065
Timon Viola <violatimon@gmail.com>
2020-04-08 09:37:16 +0200

    feat: rewrite psat silent script for oopsat

--- +util/psatSilent.m
+++ +util/psatSilent.m
@@ -1,10 +1,8 @@
-% do not display splash msg if true (1)    % feat: add -nosplash option to 
-nosplash = 1;                              %     PSAT CLI to surpress splash
 % initialize PSAT
-initpsat
+ps = psat('command_line_psat',true,'nosplash',true);
 
 % do not reload data file on pf run
-clpsat.readfile = 0;
+ps.clpsat.readfile = 0;
 
 % do not display opf message in cmw (e.g.: pf details)
-clpsat.mesg = 0;
+ps.clpsat.mesg = 0;
