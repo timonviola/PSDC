173e030a4f7f491b7f8a42a3869e802511f02ab7
Timon Viola <violatimon@gmail.com>
2020-04-09 14:58:12 +0200

    feat(hpc): add hpc test analysis

---
+++ hpc_files/hpc_par_psat_results.m
@@ -0,0 +1,52 @@
+% analyze the performance of running psat parallel on cluster
+
+t_n02 = 'Started at Thu Apr  9 11:37:40 2020 Terminated at Thu Apr  9 11:39:55 2020'; % data_set_6148049.out
+%     CPU time :                                   60.00 sec.
+%     Max Memory :                                 833 MB
+%     Average Memory :                             681.43 MB
+%     Total Requested Memory :                     196608.00 MB
+%     Delta Memory :                               195775.00 MB
+%     Max Swap :                                   -
+%     Max Processes :                              5
+%     Max Threads :                                100
+%     Run time :                                   135 sec.
+%     Turnaround time :                            137 sec.
+
+t_n03 = 'Started at Thu Apr  9 11:41:32 2020 Terminated at Thu Apr  9 11:44:13 2020'; % data_set_6148060.out
+%     CPU time :                                   62.00 sec.
+%     Max Memory :                                 832 MB
+%     Average Memory :                             720.50 MB
+%     Total Requested Memory :                     196608.00 MB
+%     Delta Memory :                               195776.00 MB
+%     Max Swap :                                   -
+%     Max Processes :                              5
+%     Max Threads :                                100
+%     Run time :                                   161 sec.
+%     Turnaround time :                            162 sec.
+
+t_n04 = 'Started at Thu Apr  9 10:37:25 2020 Terminated at Thu Apr  9 10:46:21 2020'; % data_set_6148003.out
+% Resource usage summary:
+% 
+%     CPU time :                                   73.16 sec.
+%     Max Memory :                                 885 MB
+%     Average Memory :                             791.15 MB
+%     Total Requested Memory :                     196608.00 MB
+%     Delta Memory :                               195723.00 MB
+%     Max Swap :                                   -
+%     Max Processes :                              5
+%     Max Threads :                                100
+%     Run time :                                   536 sec.
+%     Turnaround time :                            537 sec.
+
+t_n05 = 'Started at Thu Apr  9 12:06:33 2020 Terminated at Thu Apr  9 13:26:42 2020'; % out_dataSet_6148200.txt
+%     CPU time :                                   797.00 sec.
+%     Max Memory :                                 1461 MB
+%     Average Memory :                             1011.22 MB
+%     Total Requested Memory :                     196608.00 MB
+%     Delta Memory :                               195147.00 MB
+%     Max Swap :                                   -
+%     Max Processes :                              5
+%     Max Threads :                                100
+%     Run time :                                   4809 sec.
+%     Turnaround time :                            4810 sec.
+
