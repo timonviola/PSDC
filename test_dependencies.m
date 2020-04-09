efa1ae6e64ef531f39c8416aba6093ce0de58b51
Timon Viola <violatimon@gmail.com>
2020-04-09 14:57:48 +0200

    build: add oopsat version test

--- test_dependencies.m
+++ test_dependencies.m
@@ -1,6 +1,6 @@
 function test_matlab()
     if verLessThan('matlab','9.6')
-       warning('dPSE:dependencyVER','Your version of MATLAB was not tested yet, you might encounter unexpected behaviour.\nMATLAB 2019a (9.6) is recommended.') 
+       warning('PSDC:dependencyVER','Your version of MATLAB was not tested yet, you might encounter unexpected behaviour.\nMATLAB 2019a (9.6) is recommended.') 
     end
 end
 
@@ -11,6 +11,16 @@ function test_matpower()
     % if sum(idx) == 1
     mpVer = allToolboxes(idx);
     if str2num(mpVer.Version) ~= 6
-        warning('dPSE:dependencyVER','Your version of MATPOWER was not tested yet, you might encounter unexpected behaviour.\nMATPOWER 6.0 is recommended.') 
+        warning('PSDC:dependencyVER','Your version of MATPOWER was not tested yet, you might encounter unexpected behaviour.\nMATPOWER 6.0 is recommended.') 
+    end
+end
+
+function test_oopsat()
+    allToolboxes = ver();
+    tbNameFun = @(x) allToolboxes(x).Name == "OOPSAT";
+    idx = arrayfun(tbNameFun,1:numel(allToolboxes));
+    mpVer = allToolboxes(idx);
+    if str2num(mpVer.Version) < 0
+        warning('PSDC:dependencyVER','Your version of MATPOWER was not tested yet, you might encounter unexpected behaviour.\nMATPOWER 6.0 is recommended.') 
     end
 end
