032880f7ec7ecfdbd7b6d1f9b4a9960cd6a74eb3
timonviola <s182215@student.dtu.dk>
2020-04-09 14:37:45 +0200

    fix: fix matlab batch starter script wall time error

--- mStart.sh
+++ mStart.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 #BSUB -q elektro
 #BSUB -J data_set
+#BSUB -W 10:00
 #BSUB -n 24
 #BSUB -R "rusage[mem=8GB]"
-#BSUB -N s182215@student.dtu.dk
+#BSUB -N 
 #BSUB -o out_dataSet_%J.txt
 #BSUB -e Error_%J.txt
-#BUSB -W 00:10
+
+
+# ---- execute matlab script -----
+
 matlab -nodisplay -nosplash -nodesktop -r "run('~/thesis/PSDC/test_psat_par.m');exit;"
