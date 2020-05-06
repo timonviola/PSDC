5892125fd596f6cc21006e931a5ee43269014126
Timon Viola <violatimon@gmail.com>
2020-05-06 11:06:20 +0200

    fix(julia): fix opt problem normalization and RCall error on large num iterations

--- qc_relaxation.jl
+++ qc_relaxation.jl
@@ -1,116 +1,46 @@
 using PowerModels
-using JuMP
 using Ipopt
-using RCall # use RCall since hitandrun has an efficient implementation there,
-## the prefered Julia lib would be MAMBA but it does not have this sampling. See:
-## https://mambajl.readthedocs.io/en/latest/samplers.html?highlight=sample#sampling-functions
-using Random
 using DelimitedFiles
-
-function get_volume(N,A_j,b_j)
-    rSTDO = R"""
-        library(volesti)
-        A <- matrix(unlist($A_j), ncol = $N, byrow=TRUE)
-        b <- c(unlist($b_j))
-        P = Hpolytope$new(A,b)
-        volume(P)
-    """
-    return rcopy(rSTDO)
-end
-
-function sample_Rpoints(N,A_j=[],b_j=[])
-    """Uniform sampling from a convex polytope using R-volesti lib"""
-    if (isempty(A_j))
-        rSTDO = R"""
-            library(volesti)
-            A <- rbind(diag($N), -diag($N))
-            b <- cbind(matrix(1,1,$N), matrix(0,1,$N))
-            P = Hpolytope$new(A,b)
-            sample_points(P,N=1)
-        """
-        A_j = rcopy(R"A")
-        b_j = rcopy(R"b")
-        return rcopy(rSTDO), A_j, b_j
-    else
-        println("before sampling points")
-        rSTDO = R"""
-            library(volesti)
-            A <- matrix(unlist($A_j), ncol = $N, byrow=TRUE)
-            b <- c(unlist($b_j))
-            P = Hpolytope$new(A,b)
-            sample_points(P,N=1)
-        """
-        return rcopy(rSTDO)
-    end
-end
-
-function run_qc_relax(pm, number_of_iterations)
-    """ Iteratively solve modified QC-AC-OPF
-    inputs: power model and number of iterations"""
-    # Initialize variables
-    start = time();
-
-    # build optimization problem
-    vars = [pm.var[:nw][0][:pg].data; pm.var[:nw][0][:vm].data];
-    N = length(vars);
-    nFactor = JuMP.upper_bound.(vars) - JuMP.lower_bound.(vars);
-    #vars = vars./nFactor; # normalize
-    # auxiliary variables to bridge AffExpr <-> NLconstraint
-    #   See "Syntax notes" http://www.juliaopt.org/JuMP.jl/dev/nlp/#Nonlinear-Modeling-1)
-    @variable(pm.model,aux[1:N])
-    @constraint(pm.model,aux .== vars./nFactor)
-    vars = vars./nFactor; # normalize after aux is defined
-
-    # Create x_hat random sample
-    x_hat,A,b = sample_Rpoints(N) #Initial sample from N unit cube
-    print("Volume: ",get_volume(N,A,b))
-    # Build optimization problem
-    # Alter the QCCR problem
-    @variable(pm.model, r);
-    @objective(pm.model, Min, r);
-    @NLparameter(pm.model, x_hat_p[i = 1:N] == x_hat[i]) # updating the parameter has performance gain vs rebuilding the model
-    @NLconstraint(pm.model, con_sphere, sqrt(sum((aux[i]-x_hat_p[i])^2 for i in 1:N))<= r);
-    print("number of it: ", number_of_iterations)
-    for k = 1:number_of_iterations # Julia for loop has it's own scope, x_hat not accessible
-        # Execute optimization
-        result = optimize_model!(pm, optimizer=optimizer_with_attributes(Ipopt.Optimizer, "print_level" => 0))
-        # Calculate outputs
-        x_opt = JuMP.value.(vars);
-        n_normT = transpose(x_opt[:] - x_hat); # This can be simply -> objective_value(pm.model) NO, in the article that is not abs() but l^1 norm
-        println(JuMP.value(r))
-
-        if (isapprox(JuMP.value(r), 0; atol=TOLERANCE))
-            # Update results
-            A = [A; n_normT]
-            b = [b[:]; n_normT*x_opt]
-        end
-        println(size(A))
-        println(size(b))
-        x_hat = sample_Rpoints(N, A, b);
-        println("points sampled")
-        # print("Volume: ", get_volume(N,A,b))
-        JuMP.set_value.(x_hat_p, x_hat)
-        println("jump value set")
-    end
-    # println("Volume calculation...")
-    # print("Volume: ",get_volume(N,A,b))
-    return A,b
-end
+using Memento
+include("qc_relaxation_methods.jl")
 
 
 """ Run N number of iterations that executes a modified QC relaxed ACOPF to created
     separating hyper-planes and event reduce."""
-# TODO: Julia argparse - write help
+# set logger
+#PowerModels.silence()
+const LOGLEVEL = "info"
+Memento.setlevel!(Memento.getlogger(PowerModels.InfrastructureModels), "error")
+logger = Memento.config!(LOGLEVEL; fmt="[{level} | {name}]: {msg}")
+if LOGLEVEL == "debug"
+    # enable debug:
+    ENV["JULIA_DEBUG"] = "all"
+end
 # Input data: number of iterations , model name
-matpower_file_name = "case14.m"#ARGS[1];
-number_of_iterations = 1000#parse(Int64, ARGS[2]);
-TOLERANCE = 1e-4
 
-start = time();
+
+debug(logger, "Input args:")
+const matpower_file_name = "case14.m";#ARGS[1];#"case14.m"
+const number_of_iterations = 1#parse(Int64, ARGS[2]);#200
+if length(ARGS) > 2
+    out_file_name_A = ARGS[3]
+else
+    out_file_name_A = "A_lhs_mat22.csv"
+end
+if length(ARGS) > 3
+    out_file_name_B = ARGS[4]
+else
+    out_file_name_B = "b_rhs_vec22.csv"
+end
+@debug ARGS
+
+
+const start = time();
 # Load network data from file
 network_data = PowerModels.parse_file(matpower_file_name)
 
 # Run bound tightening
+info(logger, "Run obbt for $matpower_file_name.")
 network_data_tight, stats = PowerModels.run_obbt_opf!(network_data,
     optimizer_with_attributes(Ipopt.Optimizer, "print_level" => 0);
     model_constructor=ACPPowerModel, max_iter=1, time_limit=120.0);
@@ -119,15 +49,18 @@ network_data_tight, stats = PowerModels.run_obbt_opf!(network_data,
 # Initialize abstract power system model
 power_model = instantiate_model(network_data_tight, ACPPowerModel, PowerModels.build_opf)
 
+info(logger,"Run QC relaxation for $matpower_file_name.")
 A_out,b_out = run_qc_relax(power_model, number_of_iterations)
 
 
 
 # Write results to file
-writedlm( "A_lhs_mat2.csv",  A_out, ',')
-writedlm( "b_rhs_vec2.csv",  b_out, ',')
+info(logger,"Output files written:\n\t- $out_file_name_A \n\t- $out_file_name_B.")
+writedlm( out_file_name_A,  A_out, ',')
+writedlm( out_file_name_B,  b_out, ',')
+
 elapsed = time() - start
-print(string("elapsed time: ", elapsed, " seconds"))
+info(logger,"Elapsed time: $elapsed sec .")
 
 # Julia/REPL has some issues with scoping which actually not that starightforward
 # https://discourse.julialang.org/t/new-scope-solution/16707/227
---
+++ qc_relaxation_methods.jl
@@ -0,0 +1,153 @@
+# using PowerModels
+using JuMP
+import Ipopt
+using RCall # use RCall since hitandrun has an efficient implementation there,
+## the prefered Julia lib would be MAMBA but it does not have this sampling. See:
+## https://mambajl.readthedocs.io/en/latest/samplers.html?highlight=sample#sampling-functions
+
+const TOLERANCE = 1e-4
+# ================ module begin ===============================
+function create_pol(N::Integer)
+    rSTDO = R"""
+    library(volesti)
+    P <- GenCube($N,'H')
+    """
+    return rcopy(R"P$A"),rcopy(R"P$b")
+end
+
+function add_to_pol(Al::AbstractArray,bl::Union{AbstractArray{<:Number},Number})
+    R"""
+    P$A <- rbind(P$A, $Al)
+    P$b <- c(P$b, $bl)"""
+    nothing
+end
+
+function get_pol()
+    A = rcopy(R"P$A")
+    b = rcopy(R"P$b")
+    return A::Array{Float64,2}, b::Union{AbstractArray{<:Number},Number}
+end
+
+function sample_pol(number_of_samples::Integer=1)
+    samples = rcopy(R"sample_points(P,N=$number_of_samples)")
+    return samples::Array{Float64,2}
+end
+
+function get_pol_volume()
+    vol = rcopy(R"volume(P)")
+    return vol::Number
+end
+# ================ module end ===============================
+
+
+function get_volume(N,A_j,b_j)
+    rSTDO = R"""
+        library(volesti)
+        A <- matrix(unlist($A_j), ncol = $N, byrow=TRUE)
+        b <- c(unlist($b_j))
+        P = Hpolytope$new(A,b)
+        volume(P)
+    """
+    return rcopy(rSTDO)
+end
+
+function sample_Rpoints(N,A_j=[],b_j=[])
+    """Uniform sampling from a convex polytope using R-volesti lib"""
+    if (isempty(A_j))
+        rSTDO = R"""
+            library(volesti)
+            A <- rbind(diag($N), -diag($N))
+            b <- cbind(matrix(1,1,$N), matrix(0,1,$N))
+            P = Hpolytope$new(A,b)
+            sample_points(P,N=1)
+        """
+        A_j = rcopy(R"A")
+        b_j = rcopy(R"b")
+        return rcopy(rSTDO), A_j, b_j
+    else
+        try
+            @debug "before sampling points" #### THIS IS WHERE THE FUNCTION FAILS
+            rSTDO = R"""
+                library(volesti)
+                A <- matrix(unlist($A_j), ncol = $N, byrow=TRUE)
+                b <- c(unlist($b_j))
+                P = Hpolytope$new(A,b)
+                sample_points(P,N=1)
+            """
+            return rcopy(rSTDO)
+        catch e
+            @debug typeof(e)
+            @debug e
+        end
+    end
+end
+
+function run_qc_relax(pm, number_of_iterations)
+    """ Iteratively solve modified QC-AC-OPF
+    inputs: power model and number of iterations"""
+    # Initialize variables
+    start = time();
+    @info "QC relaxation tolerance set to $TOLERANCE ."
+    # build optimization problem
+    vars = [pm.var[:nw][0][:pg].data; pm.var[:nw][0][:vm].data];
+    N = length(vars);
+    #nFactor = JuMP.upper_bound.(vars) - JuMP.lower_bound.(vars);
+    nFactor = JuMP.upper_bound.(vars)
+    #vars = vars./nFactor; # normalize
+    # auxiliary variables to bridge AffExpr <-> NLconstraint
+    #   See "Syntax notes" http://www.juliaopt.org/JuMP.jl/dev/nlp/#Nonlinear-Modeling-1)
+    @variable(pm.model,aux[1:N])
+    @constraint(pm.model,aux .== vars./nFactor)
+    vars = vars./nFactor; # normalize after aux is defined
+
+    # Create x_hat random sample
+    create_pol(N) # Create initial unit cube
+    x_hat = sample_pol() # Initial sample from N unit cube
+
+        #   x_hat,A,b = sample_Rpoints(N) #Initial sample from N unit cube
+
+        #       @info "Volume:" get_volume(N,A,b)
+    # Build optimization problem
+    # Alter the QCCR problem
+    @variable(pm.model, r);
+    @objective(pm.model, Min, r);
+    @NLparameter(pm.model, x_hat_p[i = 1:N] == x_hat[i]) # updating the parameter has performance gain vs rebuilding the model
+    @NLconstraint(pm.model, con_sphere, sqrt(sum((aux[i]-x_hat_p[i])^2 for i in 1:N))<= r);
+
+    @debug "number of it: $number_of_iterations"
+    for k = 1:number_of_iterations # Julia for loop has it's own scope, x_hat not accessible
+        # Execute optimization
+        result = optimize_model!(pm, optimizer=optimizer_with_attributes(Ipopt.Optimizer, "print_level" => 0))
+        # Calculate outputs
+        x_opt = JuMP.value.(vars);
+        n_normT = transpose(x_opt[:] - x_hat); # This can be simply -> objective_value(pm.model) NO, in the article that is not abs() but l^1 norm
+        @debug "objective_value" JuMP.value(r)
+
+        if !(isapprox(JuMP.value(r), 0; atol=TOLERANCE))
+            # Update results
+            #   A = [A; n_normT]
+            #   b = [b[:]; n_normT*x_opt]
+            add_to_pol(n_normT, n_normT*x_opt)
+            @debug println(get_pol())
+        end
+    #               @debug "Size A: " size(A)
+    #               @debug "Size b: " size(b)
+        try
+    #               x_hat = sample_Rpoints(N, A, b);
+            x_hat = sample_pol()
+            @debug "points sampled"
+        catch e
+            @debug typeof(e)
+            @debug e
+            return A,b
+        end
+
+        # print("Volume: ", get_volume(N,A,b))
+        JuMP.set_value.(x_hat_p, x_hat)
+        @debug "jump value set"
+    end
+    # println("Volume calculation...")
+    # print("Volume: ",get_volume(N,A,b))
+    A,b = get_pol()
+    return A,b
+end
