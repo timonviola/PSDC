19a62e805cea89410fe91501410bb98ad67616aa
Timon Viola <violatimon@gmail.com>
2020-05-26 13:14:29 +0200

    fix: fix function scoping

--- +SmallSignalStability/+plot/compareEigenValues.m
+++ +SmallSignalStability/+plot/compareEigenValues.m
@@ -14,19 +14,21 @@ defaultAxesStyle = {'LineStyle',':','Color','0.65,0.65,0.65'};
 l.x = yline(0,defaultAxesStyle{:});
 l.y = xline(0,defaultAxesStyle{:});
 
-sh = cell(10,1);
+N = size(A_matrices,1);
+
+sh = cell(N,1);
 %cmp = colormap('winter');
 cmp = colormap('parula');
 %cmp = [cmp(1:20,:);cmp(44:end,:)];
 nStp = floor(size(cmp,1)/length(sh));
 cmp = cmp(1:nStp:end,:);
 
-for i = 1:10
-    [~,~,p] = damp_(A_matrices{i});
+for i = 1:N
+    [~,~,p] = SmallSignalStability.damp_(A_matrices{i});
     sh{i} = scatter(real(p),imag(p),'Visible','off','CData',cmp(i,:)); % TODO: use eigPlot
 end
 sh{1}.Visible = 'on';
-legend([sh{:}],string(1:10),'Location','best','NumColumns',2)
+legend([sh{:}],string(1:N),'Location','best','NumColumns',2)
 ah.XLim = [-1200 200];
 ah.YLim = [-15 15];
 
@@ -34,11 +36,11 @@ ah.YLim = [-15 15];
 % Toggle between data
 sliderX = uicontrol('Style','Slider', 'Callback',@showData, ...
    'Units','normalized', 'Position',[0.94 0.05 0.03 0.8], ...
-   'Min',1, 'Max',10, 'Value',1,'SliderStep',[.1 .1],'Tag','Ps');
+   'Min',1, 'Max',N, 'Value',1,'SliderStep',[1/N 1/N],'Tag','Ps');
 dText = uicontrol('Style','text','String','-',...
     'Units','normalized', 'Position',[0.1429 0.1262 0.3021 0.0714],'Tag','dText');
 
-[~,zeta,~] = damp_(A_matrices{1});
+[~,zeta,~] = SmallSignalStability.damp_(A_matrices{1});
 dText.String = "Damping ratio: " + max(zeta);
       
 oldVal = 0; 
@@ -49,9 +51,9 @@ oldColor = [0 0 0];
 function showData(varargin)      
       % Get current position of slider
       newVal = round(varargin{1}.Value);
-      
+      fprintf('in: %3.3f\trounded: %d \n',varargin{1}.Value,newVal)
       varargin{1}.Value = newVal;       % bound it
-      [~,zeta,~] = damp_(A_matrices{newVal});
+      [~,zeta,~] = SmallSignalStability.damp_(A_matrices{newVal});
       dText.String = "Damping ratio: " + max(zeta);
       if strcmp(varargin{1}.Tag,'Ps')
           %sh{curVal}.Visible = 'off';
