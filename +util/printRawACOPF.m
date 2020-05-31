178c2ad6f892714a1d689552ac262ce445fc55e2
Timon Viola <violatimon@gmail.com>
2020-05-31 12:36:58 +0200

    feat: add data summary utilities

---
+++ +util/printRawACOPF.m
@@ -0,0 +1,12 @@
+load +test\debug_const.mat
+acopfResults = readtable(ACOPF_SEED, 'ReadVariableNames',true);
+acopfResults = acopfResults(:,sort(acopfResults.Properties.VariableNames));
+
+idx = find(stable);
+stableR = acopfResults(idx,:);
+
+summary(stableR)
+
+% histogram(stableR.PG2,100);
+
+
