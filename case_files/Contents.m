eba9c0464573ec88e753a64c16afc1bf633a24f0
Timon Viola <violatimon@gmail.com>
2020-06-02 09:10:33 +0200

    fix: fix Contents.m

--- case_files/Contents.m
+++ case_files/Contents.m
@@ -1,15 +1,15 @@
 % CASE_FILES
 %
 % Files
-%   bus_024_mdl            - 24 BUS model, but no dynamic data is available
+%   case9                  - Power flow data for 9 bus, 3 generator case.
+%   d_009_dyn              - WSCC 9 BUS 3 machine test system
 %   case14                 - MATPOWER
 %   case14_matpower_limits - IEEE14 BUS TEST SYSTEM - generator limits set to matpower case14 lims
-%   d_009_dyn              - WSCC 9 BUS 3 machine test system
 %   d_014_dyn_mdl_pretty   - IEEE14 BUS TEST SYSTEM - commented data file
-%   d_iceland              - 189 BUS 35 GENERATORS
+%   bus_024_mdl            - 24 BUS model, but no dynamic data is available
+%   case39                 - Power flow data for 39 bus New England system.
 %   d_IEEE39bus            - IEEE39 BUS test system 10 GENERATORS
-
-
-%   case9                  - Power flow data for 9 bus, 3 generator case.
+%   d_iceland              - 189 BUS 35 GENERATORS
 %   d_IEEE68bus            - IEEE68 BUS SYSTEM 16 GENERATORS
+
 
