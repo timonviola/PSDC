7aa3c2de6c15a336055ba051ccd3175c3ac54937
Timon Viola <violatimon@gmail.com>
2020-06-08 13:14:39 +0200

    feat(util): rewrite dataSummary to function

--- +util/dataSummary.m
+++ +util/dataSummary.m
@@ -1,32 +1,37 @@
-clear
+% DATASUMMARY Checks all setpoints defined and saves the results.
+%
+% DATASUMMARY(setPointsCsv,psatFile,mpFile,varargin) Checks all setpoints
+% defined by setPointsCsv agains ACOPF and Small-signal stability criteria.
+% psatFile defines the dynamic data file and mpFile is the static data
+% file. After the checks are finished the results are saved into a .mat
+% file and the results are printed to STDOUT.
+%
+% See also UTIL.PRINTDATASUMMARY 
+
+function dataSummary(setPointsCsv,psatFile,mpFile,varargin)
 t = tic;
+readTVars = false;
+if nargin > 3
+    readTVars = varargin{1};
+end
 
-% ACOPF_SEED = '.data/case14_ACOPF.csv';
-% CASE_FILE = 'case_files/case14.m';
-% PSAT_FILE = 'case_files/d_014_dyn_mdl_pretty.m';
-ACOPF_SEED = 'set_points_case9_1.csv';%'.data/case9_2020_06_03T162628Z/case9_ACOPF.csv'; %'set_points_case9_58.csv';%'set_points_case9_100.csv';%.data/case9_ACOPF.csv';
-CASE_FILE = 'case_files/case9.m';
-PSAT_FILE = 'case_files/d_009_dyn.m';
+if ~readTVars && (contains(setPointsCsv,'ACOPF') || contains(setPointsCsv,'QCRM'))
+    warning('PSDC:utils','Input table columns will not be sorted.')
+end
+% columns are sorted already
+% variable names should be sorted if QCRM or ACOPF data is checked, if DW
+% data is checked, there is no header.
+res = readtable(setPointsCsv, 'ReadVariableNames',readTVars);
 
-% df = 'set_points_2.csv';
-% % columns are sorted already
-% dwResults = readtable(df, 'ReadVariableNames',false);
-% acopfResults = readtable(ACOPF_SEED, 'ReadVariableNames',true);
-acopfResults = readtable(ACOPF_SEED, 'ReadVariableNames',false);
-
-% Sort columns
-%acopfResults = acopfResults(:,sort(acopfResults.Properties.VariableNames));
-dwResults = acopfResults;
-
-
-
-
-
+% if setPointsCsv is QCRM/ACOPF the columns have to be sorted
+if readTVars
+    res = res(:,sort(res.Properties.VariableNames));
+end
 
 % Check for repeated data
-[C, ia, ic]= unique(dwResults,'stable');
+[C, ~, ~]= unique(res,'stable');
 
-if size(C) == size(dwResults)
+if size(C) == size(res)
     warning('some values are doubled')
 else
     fprintf('No duplicate values.\n')
@@ -34,7 +39,7 @@ end
 
 toc(t)
 
-N = size(dwResults,1);
+N = size(res,1);
 stable = nan(N,1);
 % criteria fail/pass details [PG QG VM S_{flow}]
 classDetails = cell(N,1);
@@ -43,30 +48,28 @@ dampingRatio = nan(N,1);
 
 
 % Set up cluster
-% if ispc
-%     if isempty(gcp)
-%         p = parpool('nocreate');
-%     else
-%         p = gcp();
-%     end
-% else
-%     % load the default cluster profile
-%     clust=parcluster(dccClusterProfile());
-%     numw = 16;
-%     p = parpool(clust, numw);
-% end
-% disp(p)
-% 
-% if ispc
-%     p.addAttachedFiles('c:\Users\Timon\myPSAT\psat\');
-% else
-%     p.addAttachedFiles('~/thesis/psat/');
-%     p.addAttachedFiles('~/thesis/textBar/textBar.m');
-% end
-% p.addAttachedFiles(PSAT_FILE);
-% p.addAttachedFiles(CASE_FILE);
+if ispc
+    if isempty(gcp)
+        p = parpool('nocreate');
+    else
+        p = gcp();
+    end
+else
+    % load the default cluster profile
+    clust=parcluster(dccClusterProfile());
+    numw = 16;
+    p = parpool(clust, numw);
+end
+disp(p)
 
-
+if ispc
+    p.addAttachedFiles('c:\Users\Timon\myPSAT\psat\');
+else
+    p.addAttachedFiles('~/thesis/psat/');
+    p.addAttachedFiles('~/thesis/textBar/textBar.m');
+end
+p.addAttachedFiles(psatFile);
+p.addAttachedFiles(mpFile);
 
 
 % Check all SP's small signal stability
@@ -75,14 +78,16 @@ pw = textBar(N,'Parallel checks');
 
 
 t2 = tic;
-for i = 1:N
-    [stable(i), classDetails{i}, dampingRatio(i)] = ...
-        DirectedWalks.checkSetpoint(dwResults{i,:},PSAT_FILE,CASE_FILE);
+parfor i = 1:N
+    [stable(i), classDetails{i}, dampingRatio(i)] = ... 
+        DirectedWalks.checkSetpoint(res{i,:},psatFile,mpFile);
     increment(pw)
 end
 delete(pw)
 toc(t2)
-save('data_set_points_dw1.mat','stable','classDetails','dampingRatio')
+
+fname = strsplit(setPointsCsv,'.');
+save([fname{1}  '_summary.mat'],'stable','classDetails','dampingRatio')
 
 %% print
-util.printDataSummary
+util.printDataSummary(stable, classDetails)
