5b85a862a8818acec885bebcc27133021df84974
Timon Viola <violatimon@gmail.com>
2020-06-08 17:45:14 +0200

    feat: extend hpc config help script

--- hpc_files/cluster_config.m
+++ hpc_files/cluster_config.m
@@ -15,10 +15,19 @@ c.AdditionalProperties.MemUsage = '2GB';
 c.AdditionalProperties.ProcsPerNode = 8;
 % Specify the walltime (e.g. 5 hours)
 c.AdditionalProperties.WallTime = '05:00';
+% set que name so we don't end up in the common hpc
+c.AdditionalProperties.QueueName='elektro';
 
 % save as default profile
 c.saveProfile
-% List all available profiles
+%% List all available profiles
 [ALLPROFILES, DEFAULTPROFILE] = parallel.clusterProfiles;
 
+%% Export profile to file
+parallel.exportProfile('MyProfile','MyExportedProfileFileName')
+% will be saved to current dir with .settings extension
+
+
 % Set up batch script according to http://www.hpc.dtu.dk/?page_id=2021#Distrib_Run
+% I saved my cluster as clusterProfileElektro2019 to
+% elektroProfile.settings
