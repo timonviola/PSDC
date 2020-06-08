535122242bb51f68d399b2fd2c19bc01e4e3079f
timonviola <s182215@student.dtu.dk>
2020-06-08 17:52:52 +0200

    feat: add directed walk job script

---
+++ run_dw.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#BSUB -q elektro
+#BSUB -J dw_force
+#BSUB -W 12:00
+#BSUB -n 1 
+#BSUB -R "rusage[mem=2GB]"
+#BSUB -N 
+#BSUB -o out_dw_%J.txt
+#BSUB -e error_dw_%J.txt
+
+matlab -nodisplay -nosplash -nodesktop -r "run('~/thesis/PSDC/m3_dw.m');exit;"
