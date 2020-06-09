46c89a1d20f05fc7bd0efc13b6a8d222e98cf5e8
timonviola <s182215@student.dtu.dk>
2020-06-09 14:05:02 +0200

    chore: change summary run

--- run_sum.m
+++ run_sum.m
@@ -1,15 +1,16 @@
-ACOPF_SEED = 'set_points_case9_1.csv';
+ACOPF_SEED = '.data/case9_2020_06_08T144804Z/case9_ACOPF.csv'; %'set_points_case9_1.csv';
 CASE_FILE = 'case_files/case9.m';
 PSAT_FILE = 'case_files/d_009_dyn.m';
 %
-%res = readtable('set_points_casecase9_1.csv','ReadVariableNames',false);
-res = [];
-fl = dir('./*casecase9_*.csv');
-N = size(fl,1);
-disp(['Concatenating ' num2str(N) ' files.'])
-for i = 1:N
-	res = [res; readtable(fl(i).name,'ReadVariableNames',false)];
-end
-writematrix(res{:,:}, 'unified_case9.csv')
+%
+%res = [];
+%fl = dir('./*casecase9_*.csv');
+%N = size(fl,1);
+%disp(['Concatenating ' num2str(N) ' files.'])
+%for i = 1:N
+%	res = [res; readtable(fl(i).name,'ReadVariableNames',false)];
+%end
+%writematrix(res{:,:}, 'unified_case9.csv')
+
 util.add_dependencies
-util.dataSummary('unified_case9.csv', PSAT_FILE, CASE_FILE)
+util.dataSummary(ACOPF_SEED, PSAT_FILE, CASE_FILE,true)
