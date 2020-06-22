6b461f9f716c4ee6f9e7134064a8db204ff6b0cf
Timon Viola <violatimon@gmail.com>
2020-06-22 22:14:58 +0200

    fix(dw): fix dw functions to accomodate new input interface

--- +DirectedWalks/dwg_f.m
+++ +DirectedWalks/dwg_f.m
@@ -1,11 +1,23 @@
 function NEW_DS_POINTS = dwg_f(setPoint, PSAT_FILE, CASE_FILE,varargin)
 
-PRINT = false;
-if nargin > 3
-    if strcmp(varargin{1},'print')
-        PRINT = true;
-    end
-end
+
+zetaMinDefault = 0.03;
+printDefault = false;
+p = inputParser;
+addRequired(p,'setPoint',@(x) isnumeric(x))
+addRequired(p,'PSAT_FILE')
+addRequired(p,'CASE_FILE')
+addOptional(p,'zetaMin',zetaMinDefault , @(x)isnumeric(x))
+addParameter(p,'print',printDefault, @(x)islogical(x))
+
+parse(p,setPoint, PSAT_FILE, CASE_FILE, varargin{:})
+setPoint = p.Results.setPoint;
+PSAT_FILE = p.Results.PSAT_FILE;
+CASE_FILE = p.Results.CASE_FILE;
+zetaMin = p.Results.zetaMin;
+PRINT = p.Results.print;
+
+
 import DirectedWalks.*
 
 % ------ DW hyper parameters ------
@@ -21,9 +33,10 @@ HICStepSizeScale = 0.005; % TODO: make sure that its consistents with alpha
 % MATPOWER INIT
 MPC = loadcase(CASE_FILE);
 % PMAX vector of generators
-PMAX = 9;
+PMAX = 9;VMAX = 12;
 gIdx = util.getGenList(MPC);
-gMaxVec = MPC.gen(gIdx,PMAX)./MPC.baseMVA; % TODO: remove slack
+allGIdx = MPC.gen(:,1);
+gMaxVec = [MPC.gen(gIdx,PMAX)./MPC.baseMVA; MPC.bus(allGIdx,VMAX)]; % TODO: remove slack
 % PSAT INIT
 ps = psat('command_line_psat',true,'nosplash',true);
 ps.clpsat.mesg = 0;
@@ -47,11 +60,11 @@ buff = zeros(1,nBuff);
 NEW_DS_POINTS = [];%cell{K_max, nDim};
 
 % get current zeta
-[~, curDR] = SmallSignalStability.checkSmallSignalStability(.03, ps.LA.a);
+[~, curDR] = SmallSignalStability.checkSmallSignalStability(zetaMin, ps.LA.a);
 % number of generators except slack
 nPG = size(ps.PV.store,1);
 DR = curDR;
-dist = getDist(DR);
+dist = getDist(DR,zetaMin);
 buff(nBuff) = DR;
 nSP = setPoint;
 ps.PVSet(nSP);
@@ -83,7 +96,7 @@ end
 
 EXIT = false;
 if PRINT
-    [fig, ax, prop] = plot.plotDWInit('legend',true); % ax
+    [fig, ax, prop] = plot.plotDWInit('zetaMin',zetaMin,'legend',true); % ax
     [etH, et] = plot.addTimeElapsedBox(fig);
     drLine = cell(K_max,1);
     drLine{i} = plot.plotDwUpdate(ax,0,DR,prop,nSP);
@@ -101,19 +114,20 @@ while i <= K_max
         ps.runpsat('pf');
         ps.fm_abcd();
         % get new DR
-        [~, DR] = SmallSignalStability.checkSmallSignalStability(.03, ps.LA.a);
+        [~, DR] = SmallSignalStability.checkSmallSignalStability(zetaMin, ps.LA.a);
         buff = circshift(buff,-1);
         buff(nBuff) = DR;
-        dist = getDist(DR);
+        dist = getDist(DR,zetaMin);
+        
+        if PRINT
+            etH.String = ['Elapsed time: ' util.getTimeElapsed(et)];
+            drLine{i} = plot.plotDwUpdate(ax,i,DR,prop,nSP);
         %         fprintf('nSP ')
         %         disp(nSP)
         %         fprintf('DR ')
         %         disp(DR)
         %         fprintf('dist ')
         %         disp(dist)
-        if PRINT
-            etH.String = ['Elapsed time: ' util.getTimeElapsed(et)];
-            drLine{i} = plot.plotDwUpdate(ax,i,DR,prop,nSP);
         end
         i = i+1;
         if ~(any(diff(buff)))
@@ -136,33 +150,34 @@ while i <= K_max
     % we are in HIC
     % take samples around the current point
     NEW_DS_POINTS = [NEW_DS_POINTS; nSP];                   %#ok FOR NOW DO NOT PREALLOCATE
-    NEW_DS_POINTS = [NEW_DS_POINTS; getHICSamples(nPG,nSP,MPC)]; %#ok FOR NOW DO NOT PREALLOCATE
+    NEW_DS_POINTS = [NEW_DS_POINTS; getHICSamples(nSP,MPC)]; %#ok FOR NOW DO NOT PREALLOCATE
     % take the next step with the minimum step size -> no step size calc
     % take the next step to the direction where the DR stays the same/close
     %     gradDir = getHICStepDir(ps, nSP, DR);
-    gradDir = getStepDir(ps, nSP, DR, 'print',PRINT);
+    gradDir = getStepDir(ps, nSP, DR,'zetaMin',zetaMin,'print',PRINT);
     alpha_k = gMaxVec.*HICStepSizeScale;
     % calculate the new set point
-    nSP(1:nPG) = nSP(1:nPG) + (alpha_k .* gradDir)';
+    nSP = nSP + (alpha_k .* gradDir)';
     % take the step: set psat object to new set point values
     ps.PSet(nSP);
     ps.runpsat('pf');
     ps.fm_abcd();
     % get new DR
-    [~, DR] = SmallSignalStability.checkSmallSignalStability(.03,ps.LA.a);
-    dist = getDist(DR);
+    [~, DR] = SmallSignalStability.checkSmallSignalStability(zetaMin,ps.LA.a);
+    dist = getDist(DR,zetaMin);
     if PRINT
         etH.String = ['Elapsed time: ' util.getTimeElapsed(et)];
         drLine{i} = plot.plotDwUpdate(ax,i,DR,feasProp,nSP);
-    end
     %     fprintf('nSP ')
     %     disp(nSP)
     %     fprintf('DR ')
     %     disp(DR)
     %     fprintf('dist ')
     %     disp(dist)
+    end
+   
     
-    [LIDX, ~]=ismember(nSP(1:nPG), NEW_DS_POINTS(:,1:nPG),'rows');
+    [LIDX, ~]=ismember(nSP, NEW_DS_POINTS,'rows');
     if LIDX
         warning('PSDC:DW','New setpoint already in data set. Exiting dw.')
         break
