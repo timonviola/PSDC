7f015ae4bcfb6d64c789b645a408fe495a6c8c06
Timon Viola <violatimon@gmail.com>
2020-06-22 17:01:43 +0200

    feat(test): fit case39,68 to eig val comp test

--- +test/test_eigenvalue_comparision.m
+++ +test/test_eigenvalue_comparision.m
@@ -1,5 +1,7 @@
-N = 8;
-caseName = 'case_files/d_IEEE68bus.m';
+N = 20;
+PG = 4;
+PV = 5;
+caseName = 'case_files/d_case68.m';
 
 voltages = cell(N,1);       % voltages
 Asys = cell(N,1);           % system matrices
@@ -13,14 +15,18 @@ ps.Settings.pv2pq=1;
 ps.runpsat(caseName,'data');
 ps.runpsat('pf')
 
+% loadData = ps.PQ.store;
+% loadData(:,PG) = loadData(:,PG).*0.5;
+% ps.PQ.store = loadData;
+% ps.runpsat('pf');
 
 nPg = size(ps.PV.store,1);
-PG = 4;
+
 % Update values
 for i =1:N
     % Change generator Pg output
     generatorData = ps.PV.store;
-    generatorData(:,PG) = generatorData(:,PG).*0.95;%1.05;%rand(nPg,1)*2;
+    generatorData(:,PV) = generatorData(:,PV).*0.99;%1.05;%rand(nPg,1)*2;
     ps.PV.store = generatorData;
     
     ps.runpsat('pf');
@@ -30,4 +36,7 @@ for i =1:N
     Asys{i} = ps.LA.a; 
 end
 
+warning('off','PSDC:SSS:eig')
 SmallSignalStability.plot.compareEigenValues(Asys)
+warning('on','PSDC:SSS:eig')
+SmallSignalStability.plot.plotStabMargin(0.03);
