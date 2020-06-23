2f99c3dc187a20575804de82cc1bebbd0bc1a669
Timon Viola <violatimon@gmail.com>
2020-06-23 12:51:27 +0200

    feat: add better SH script printing to stage2

--- m2_genSh.m
+++ m2_genSh.m
@@ -5,7 +5,7 @@ output_fname = [OUT_DIR filesep CASE_NAME '_ACOPF.csv'];
 
 
 fileID = fopen([OUT_DIR filesep 'j_opf.sh'],'w');
-writeSH(fileID)
+writeSH(fileID,CASE_NAME)
 
 fprintf(fileID, strrep(['julia -p 4 opf_par.jl "' output_fname '" "' ...
     case_file '" "' input_file '"'],'\','/'));
@@ -15,17 +15,17 @@ fclose(fileID);
 disp('Run:')
 disp(['bsub < ' strrep([OUT_DIR, filesep], '\','/') 'j_opf.sh'])
 
-function writeSH(fid)
+function writeSH(fid,CASE_NAME)
 sh_cont = [...
     "#!/bin/sh"
     "#BSUB -q elektro"
-    "#BSUB -J j_opf_par"
+    "#BSUB -J opf_"+string(CASE_NAME)
     "#BSUB -W 02:00"
     "#BSUB -n 10"
     '#BSUB -R "rusage[mem=8GB]"'
     "#BSUB -N"
-    "#BSUB -o out_temp_%J.txt"
-    "#BSUB -e error_temp_%J.txt"
+    "#BSUB -o out_opf_temp_%J.txt"
+    "#BSUB -e error_opf_temp_%J.txt"
     ""
     "module load julia/1.1.1"
 ];
